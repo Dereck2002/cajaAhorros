ea7cb3071d0ad92a636ee48e3d2d8ba78f29771c
Juan Carlos Martinez Toala <jc.martineztoala@gmail.com>
2025-05-29 23:54:44 -0500

    se arreglo el problema de que no se visualisaban las fechas y la imagen en editar socio

--- cajaAhorros/app_cajaAhorros/forms.py
+++ cajaAhorros/app_cajaAhorros/forms.py
@@ -6,22 +6,23 @@ class SocioForm(forms.ModelForm):
         model = Socio
         fields = ('cedula', 'nombre', 'apellido', 'fecha_nacimiento', 'foto', 'fecha_ingreso')
         widgets = {
-            'fecha_nacimiento': forms.DateInput( 
-                attrs={'type': 'date', 'class': 'form-control'}
-                ),
-            'fecha_ingreso': forms.DateInput( 
-                attrs={'type': 'date', 'class': 'form-control'}
-                ),
+            'fecha_nacimiento': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'},
+                format='%Y-%m-%d'
+            ),
+            'fecha_ingreso': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'},
+                format='%Y-%m-%d'
+            ),
             'foto': forms.FileInput(attrs={'accept': 'image/*', 'class': 'form-control'}),
             'cedula': forms.TextInput(attrs={'min': 0, 'class': 'form-control'}),
-            'nombre':forms.TextInput(attrs={'placeholder': 'Nombre', 'class': 'form-control'}),
+            'nombre': forms.TextInput(attrs={'placeholder': 'Nombre', 'class': 'form-control'}),
             'apellido': forms.TextInput(attrs={'placeholder': 'Apellido', 'class': 'form-control'}),
         }
-        labels = {
-            'cedula': 'Cédula',
-            'nombre': 'Nombre',
-            'apellido': 'Apellido',
-            'fecha_nacimiento': 'Fecha de nacimiento',
-            'foto': 'Foto del socio',
-            'fecha_ingreso': 'Fecha de ingreso',
-        }
+
+    # Necesario para que las fechas se muestren al editar
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in ['fecha_nacimiento', 'fecha_ingreso']:
+            if self.instance and getattr(self.instance, field):
+                self.fields[field].initial = getattr(self.instance, field).strftime('%Y-%m-%d')
