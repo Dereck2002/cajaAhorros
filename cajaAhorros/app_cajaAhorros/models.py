aaac30e2262a8b213e77a6cc58272ee46cb77a59
Dereck Jaramillo <jaramilloderek13@gmail.com>
2025-06-14 00:17:35 -0500

    exportacion de pdf con firma

--- cajaAhorros/app_cajaAhorros/models.py
+++ cajaAhorros/app_cajaAhorros/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Rol(models.Model):
-    nombre = models.CharField(max_length=50, unique=True)
-    descripcion = models.TextField(blank=True, null=True)
-    activo = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.nombre
-
 class Socio(models.Model):
     cedula = models.CharField(max_length=15, unique=True)
     nombre = models.CharField(max_length=100)
